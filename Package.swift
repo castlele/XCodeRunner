50264ca555cce23449c748f9ef75333a4827d9a4
Nikita Semenov <castlelecs@gmail.com>
2022-10-31 20:31:16 +0300

    feat: added dependency

--- Package.swift
+++ Package.swift
@@ -5,18 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "XCodeRunner",
+    platforms: [
+        .macOS(.v12),
+    ],
+    products: [
+        .executable(name: "xrunner", targets: ["XCodeRunner"])
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/castlele/CLArgumentsParser.git", branch: "master")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .executableTarget(
-            name: "XCodeRunner",
-            dependencies: []),
-        .testTarget(
-            name: "XCodeRunnerTests",
-            dependencies: ["XCodeRunner"]),
+        .executableTarget(name: "XCodeRunner", dependencies: ["CLArgumentsParser"]),
+        .testTarget(name: "XCodeRunnerTests", dependencies: ["XCodeRunner"]),
     ]
 )
